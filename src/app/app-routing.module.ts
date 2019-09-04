14e45476904fec34c9586ec3143c770a8d56bf5e
siew.pheng.lew <siew.pheng.lew@dell.com>
2019-09-04 14:40:18 +0800

    updated2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { Page2Component } from './page2/page2.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "mainpage", component: MainpageComponent},
+  {path: "page2", component: Page2Component},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
+import { Page2Component } from './page2/page2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainpageComponent
+    MainpageComponent,
+    Page2Component
   ],
   imports: [
     BrowserModule,
